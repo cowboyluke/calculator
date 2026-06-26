dc5ff9255d128f65941128576eb4049ba424c2a5
cowboyluke <189388733+cowboyluke@users.noreply.github.com>
2026-06-26 12:56:28 -0700

    Finish max char on screen function and repetative equals function. Some bugs at extreme values still, however ~90% working. Util Functions still not added.

--- script.js
+++ script.js
@@ -20,8 +20,14 @@ function divide(a,b) {
 let num1 = undefined;
 let num2 = undefined;
 let operand = undefined;
+
 let numberCounter = 0;
 let nextNumber = false;
+
+let equalsCounter = 0;
+let num1e = undefined;
+let num2e = undefined;
+let operande = undefined
 
 
 function operate(n1, n2, op) {
@@ -47,28 +53,64 @@ function operate(n1, n2, op) {
 function displayResults(number) {
     if (String(number).length < 11) {
         display.textContent = String(number);
-    } else if (number > 0) { //big number scientific exponent
+    } else if (Math.abs(number) > (1 * 10 ** 9)) { //big number scientific exponent
         let base10decimal = number;
         let exponent = 0;
-        for (let i = 0; i < (String(number).length - 1); i++) {
+        let decimalPlaces = String(number).length;
+        for (let i = 0; i < (decimalPlaces - 1); i++) {
             base10decimal = base10decimal / 10;
             exponent++;
         };
         base10decimal = (Math.round(base10decimal*1000000))/1000000; //rounds to 6 digits, plus the e+99, makes it total 10 digits
         display.textContent = String(base10decimal) + "e+" + String(exponent);
-    } else if (number < 0) { //small number scientific exponent
-
-    }
+    } else if (Math.abs(number) < (1 * 10 ** -9)) { //small number scientific exponent
+        let base10decimal = number;
+        let exponent = 0;
+        let decimalPlaces = String(number).length;
+        if (decimalPlaces > 100) {
+            decimalPlaces = 100; //rounds off irrational numbers
+        }
+        for (let i = 0; i < (decimalPlaces - 1); i++) {
+            base10decimal = base10decimal * 10;
+            exponent++;
+        };
+        base10decimal = (Math.round(base10decimal*1000000))/1000000; //rounds to 6 digits, plus the e+99, makes it total 10 digits
+        display.textContent = String(base10decimal) + "e-" + String(exponent);
+    } else {
+            if (Math.abs(number) > 1) { //if it has something in the one's digit
+                let intStr = String(number).slice(0, String(number).indexOf("."));
+                let decStr = String(number).slice(String(number).indexOf("."));
+                decStr = "0" + decStr;
+                let decRounding = (10 - intStr.length) - 1;
+                let dec = (Math.round(Number(decStr)*10**decRounding))/10**decRounding;
+                display.textContent = String(Number(intStr) + dec);
+            } else {    //if its one's digit is zero
+                display.textContent = String((Math.round(number*(10**9)))/(10**9));
+            } 
+        }
 }
 
 function getNumber(str) {
     if (str.includes("e+")) {
         let decimal = str.slice(0, str.indexOf("e"));
         let exponent = str.slice(str.indexOf("+") +1); //done logic to create base 10, working on logic now to grab base 10. once its done will convert to negative exponents too
+        decimal = Number(decimal);
+        exponent = Number(exponent);
+        return  decimal*(10**exponent);
     } else if (str.includes("e-")) {
-
+        let decimal = str.slice(0, str.indexOf("e"));
+        let exponent = str.slice(str.indexOf("-") +1); //done logic to create base 10, working on logic now to grab base 10. once its done will convert to negative exponents too
+        decimal = Number(decimal);
+        exponent = Number(exponent) * (-1);
+        return  decimal*(10**exponent);
     } else {
-    return Number(str);
+        return Number(str);
+    }
+}
+
+function typeToScreen(str) {
+    if (display.textContent.length < 10) {
+        display.textContent += str;
     }
 }
 
@@ -115,109 +157,113 @@ numButton.forEach(btn => {
 
 
 one.addEventListener("click", () => {
-    display.textContent += "1";
+    typeToScreen("1");
 });
 two.addEventListener("click", () => {
-    display.textContent += "2";
+    typeToScreen("2");
 });
 three.addEventListener("click", () => {
-    display.textContent += "3";
+    typeToScreen("3");
 });
 four.addEventListener("click", () => {
-    display.textContent += "4";
+    typeToScreen("4");
 });
 five.addEventListener("click", () => {
-    display.textContent += "5";
+    typeToScreen("5");
 });
 six.addEventListener("click", () => {
-    display.textContent += "6";
+    typeToScreen("6");
 });
 seven.addEventListener("click", () => {
-    display.textContent += "7";
+    typeToScreen("7");
 });
 eight.addEventListener("click", () => {
-    display.textContent += "8";
+    typeToScreen("8");
 });
 nine.addEventListener("click", () => {
-    display.textContent += "9";
+    typeToScreen("9");
 });
 zero.addEventListener("click", () => {
-    display.textContent += "0";
+    typeToScreen("0");
 });
 
 
 multiplier.addEventListener("click", () => {
     if (numberCounter < 1) {
         num1 = getNumber(display.textContent);    
-        operand = "*";
-        nextNumber = true;
-        numberCounter++;
     } else {
         num2 = getNumber(display.textContent);
-        display.textContent = String(operate(num1, num2, operand));
-        operand = "*";
+        displayResults(operate(num1, num2, operand));
         num1 = getNumber(display.textContent);
-        num2 = undefined;    
+        num2 = undefined;
+
+    }        
         nextNumber = true;
         numberCounter++;
-    }
+        operand = "*";
+        operande = operand; //for repetative equals pressing
+        equalsCounter = 0;
 });
 divider.addEventListener("click", () => {
     if (numberCounter < 1) {
         num1 = getNumber(display.textContent);    
-        operand = "/";
-        nextNumber = true;
-        numberCounter++;
     } else {
         num2 = getNumber(display.textContent);
-        display.textContent = String(operate(num1, num2, operand));
-        operand = "/";
+        displayResults(operate(num1, num2, operand));
         num1 = getNumber(display.textContent);
         num2 = undefined;    
+    }
         nextNumber = true;
         numberCounter++;
-    }
+        operand = "/";
+        operande = operand;
+        equalsCounter = 0;
 });
 plus.addEventListener("click", () => {
     if (numberCounter < 1) {
         num1 = getNumber(display.textContent);    
-        operand = "+";
-        nextNumber = true;
-        numberCounter++;
     } else {
         num2 = getNumber(display.textContent);
-        display.textContent = String(operate(num1, num2, operand));
-        operand = "+";
+        displayResults(operate(num1, num2, operand));
         num1 = getNumber(display.textContent);
         num2 = undefined;    
+    }
         nextNumber = true;
         numberCounter++;
-    }
+        operand = "+";
+        operande = operand;
+        equalsCounter = 0;
 });
 minus.addEventListener("click", () => {
     if (numberCounter < 1) {
         num1 = getNumber(display.textContent);    
-        operand = "-";
-        nextNumber = true;
-        numberCounter++;
     } else {
         num2 = getNumber(display.textContent);
-        display.textContent = String(operate(num1, num2, operand));
-        operand = "-";
+        displayResults(operate(num1, num2, operand));
         num1 = getNumber(display.textContent);
         num2 = undefined;    
+    }
         nextNumber = true;
         numberCounter++;
-    }
+        operand = "-";
+        operande = operand;
+        equalsCounter = 0;
 });
 
 equals.addEventListener("click", () => { //want to add the ability to keep pressing this and repeating the operation
-        num2 = getNumber(display.textContent);
-        display.textContent = String(operate(num1, num2, operand))
-        num1 = undefined;
-        num2 = undefined;
-        operand = undefined;    
-        numberCounter = 0;
+        if (equalsCounter < 1) {
+            num2 = getNumber(display.textContent);
+            num2e = num2;
+            displayResults(operate(num1, num2, operand));
+            equalsCounter++;
+        } else { //the repetative equals pressing case
+            num1e = getNumber(display.textContent);
+            displayResults(operate(num1e, num2e, operande));
+        }
+            num1 = undefined;
+            num2 = undefined;
+            operand = undefined;    
+            numberCounter = 0;
 });
 
 //next steps are to add basic functionality to all the dark grey buttons
